64e3b2d010fb939e538f001188aa47dc48632f19
Jason <68907860+JJTheDude7@users.noreply.github.com>
2021-12-04 22:55:25 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function clearAll()
+{
+	document.getElementById("txtName").value = "";
+	document.getElementById("txtEmail").value = "";
+	document.getElementById("txtSubject").value = "";
+	document.getElementById("txtComment").value = "";
+	alert("Message sent! You will be recieving an email from one of our workers on your mail account, thank you.")
+}
+
+function darkMode()
+{
+	
+}
